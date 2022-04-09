98298b4b608660d90bf58067f802e6952338f7e3
motswanax <motswanax@users.noreply.github.com>
2022-04-09 23:29:16 +0200

    Handling http get requests.

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "çourse1" },
+  { id: 2, name: "çourse2" },
+  { id: 3, name: "çourse3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+
+  res.send(course);
 });
 
 // Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
-});
