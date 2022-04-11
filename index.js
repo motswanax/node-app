a51dc5c9cb2fe511c118c4ce7b82aeed88d2b1c0
motswanax <motswanax@users.noreply.github.com>
2022-04-11 10:34:52 +0200

    Added post request handling.

--- index.js
+++ index.js
@@ -21,18 +21,15 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found");
+    return res.status(404).send("The course with the given ID was not found");
 
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
-  
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = { id: courses.length + 1, name: req.body.name };
   courses.push(course);
@@ -45,21 +42,29 @@ app.put("/api/courses/:id", (req, res) => {
 
   // Return 404 if not found
   if (!course)
-    res.status(404).send("The course with the given ID was not found");
+    return res.status(404).send("The course with the given ID was not found");
 
   // Validate
   const { error } = validateCourse(req.body);
 
   // If invalid, return 400 - Bad Request
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   // Update course
   course.name = req.body.name;
 
   // Return updated course
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
   res.send(course);
 });
 
